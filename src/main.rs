fb99904e6502ea4d3689aa4285e7fd69535e1421
ilya.makarov <ilya.makarov@arcadia.spb.ru>
2021-09-26 23:43:08 +0700

    Divide draw

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ extern crate piston_window;
 use piston_window::*;
 use std::path::Path;
 use polynom::structs::*;
+use polynom::draw_lines::*;
 
 
-static LINE_COLOR:[f32; 4] = [0.0, 1., 0.0, 1.0];
-static LINE_RADIUS:f64 = 1.;
 static WIDTH:f64 = 600.;
 static HEIGHT:f64 = 300.;
 
@@ -134,55 +133,4 @@ fn cast_enum(view_type: &LineViewType) -> &'static str {
         LineViewType::Straight => return "Straight",
         LineViewType::Lagrange => return "Lagrange",
     }
-}
-
-fn draw_straight<G: Graphics>(points: &Vec<[f64; 2]>, transform: [[f64; 3]; 2], g: &mut G) {
-    let l = points.len();
-    if l > 1 {
-        for _i in 1..=l-1 {
-            line(LINE_COLOR,
-                LINE_RADIUS,
-                [points[_i-1][0], points[_i-1][1], points[_i][0], points[_i][1]],
-                transform, g);
-        }
-    }
-}
-
-fn draw_lagrange<G: Graphics>(points: &Vec<[f64; 2]>, transform: [[f64; 3]; 2], g: &mut G) {
-    let dx = 2.0;
-    let l = points.len();
-    if l > 1 {
-        let right = points[l-1][0];
-        let mut x = points[0][0];
-        let mut cur_line = [x, points[0][1], x, points[0][1]];
-        while x < right {
-            cur_line[0] = cur_line[2];
-            cur_line[1] = cur_line[3];
-            let y = lagrange_func(points, x);
-            cur_line[2] = x;
-            cur_line[3] = y;
-            line(LINE_COLOR,LINE_RADIUS, cur_line,transform, g);
-            x += dx;
-        }
-    }
-}
-
-fn lagrange_func(points: &Vec<[f64; 2]>, x:f64) -> f64 {
-    let mut result = 0.;
-    for _i in 1..=points.len() {
-        let i = _i - 1;
-        result += points[i][1] * lagrange_basis_func(points, x, i);
-    }
-    return result;
-}
-
-fn lagrange_basis_func(points: &Vec<[f64; 2]>, x:f64, i:usize) -> f64 {
-    let mut result = 1.;
-    for _i in 1..=points.len() {
-        let j = _i - 1;
-        if j != i {
-            result *= (x - points[j][0]) / (points[i][0] - points[j][0]);
-        }
-    }
-    return result;
 }
--- src/polynom/mod.rs
+++ src/polynom/mod.rs
@@ -1,1 +1,2 @@
 pub mod structs;
+pub mod draw_lines;
