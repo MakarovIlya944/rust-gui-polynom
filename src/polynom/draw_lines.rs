c44d7bf4aabb9f3f3bf509477e4ebbf7506f1822
ilya.makarov <ilya.makarov@arcadia.spb.ru>
2021-10-01 15:49:03 +0700

    Add hermite

--- src/polynom/draw_lines.rs
+++ src/polynom/draw_lines.rs
@@ -2,15 +2,52 @@ extern crate piston_window;
 use piston_window::*;
 static LINE_COLOR:[f32; 4] = [0.0, 1., 0.0, 1.0];
 static LINE_RADIUS:f64 = 1.;
+static LINE_DX:f64 = 10.;
+static LINE_DT:f64 = 0.1;
 
 pub fn draw_hermite<G: Graphics>(points: &Vec<[f64; 2]>, transform: [[f64; 3]; 2], g: &mut G) {
+    // (2*t*t*t - 3*t*t + 1) f0
+    // (t*t*t - 2*t*t + t) df0
+    // (-2*t*t*t + 3*t*t) f1
+    // (t*t*t - t*t) df1
     let l = points.len();
     if l > 1 {
+        let mut dist = 0.;
+        let mut t = 0.;
+        let mut df0 = 0.;
+        let mut df1 = 0.;
+        let mut x = 0.;
+        let mut y = 0.;
+        let mut cur_line = [points[0][0], points[0][1], points[0][0], points[0][1]];
         for _i in 1..=l-1 {
-            line(LINE_COLOR,
-                LINE_RADIUS,
-                [points[_i-1][0], points[_i-1][1], points[_i][0], points[_i][1]],
-                transform, g);
+            t = 0.;
+            dist = points[_i][0] - points[_i-1][0];
+            while t < 1. {
+                cur_line[0] = cur_line[2];
+                cur_line[1] = cur_line[3];
+
+                df0 = (points[_i][1] - points[_i-1][1]) / dist;
+                df1 = df0;
+                if _i == 1 {
+                    df0 = 0.;
+                } else if _i == l-1 {
+                    df1 = 0.;
+                }
+                let t2 = t*t;
+                let t3 = t2 * t;
+                
+                x = t * dist + points[_i-1][0];
+                y = (2.*t3 - 3.*t2 + 1.) * points[_i-1][1] +
+                (t3 - 2.*t2 + t) * df0 +
+                (-2.*t3 + 3.*t2) * points[_i][1] +
+                (t3 - t2) * df1;
+
+                cur_line[2] = x;
+                cur_line[3] = y;
+                line(LINE_COLOR,LINE_RADIUS, cur_line,transform, g);
+
+                t += LINE_DT;
+            }
         }
     }
 }
@@ -28,7 +65,6 @@ pub fn draw_straight<G: Graphics>(points: &Vec<[f64; 2]>, transform: [[f64; 3]; 
 }
 
 pub fn draw_lagrange<G: Graphics>(points: &Vec<[f64; 2]>, transform: [[f64; 3]; 2], g: &mut G) {
-    let dx = 2.0;
     let l = points.len();
     if l > 1 {
         let right = points[l-1][0];
@@ -41,7 +77,7 @@ pub fn draw_lagrange<G: Graphics>(points: &Vec<[f64; 2]>, transform: [[f64; 3]; 
             cur_line[2] = x;
             cur_line[3] = y;
             line(LINE_COLOR,LINE_RADIUS, cur_line,transform, g);
-            x += dx;
+            x += LINE_DX;
         }
     }
 }
